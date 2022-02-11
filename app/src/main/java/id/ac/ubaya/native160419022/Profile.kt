a30b738d577c1ff741227cca474d49c4fa3346c5
NUR FITRIANA <s160419069@microsoft.ubaya.ac.id>
2022-02-11 23:38:41 +0700

    Profile class added

---
+++ app/src/main/java/id/ac/ubaya/native160419022/Profile.kt
@@ -0,0 +1,7 @@
+package id.ac.ubaya.native160419022
+
+class Profile(var username:String, var email:String, var alamat:String) {
+    override fun toString(): String {
+        return super.toString()
+    }
+}
